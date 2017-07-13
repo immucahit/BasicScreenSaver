fbc1d3e809d32981c641ec50f252ca57be922dd7
Mücahit Bayraktar <immucahit@outlook.com>
2017-07-13 22:26:50 +0300

    files

---
+++ Configuration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic
+{
+    public partial class Configuration : Form
+    {
+        Font _font;
+        FontDialog _fontDialog;
+        public Configuration()
+        {
+            InitializeComponent();
+            _fontDialog = new FontDialog();
+        }
+
+        private void FontButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = _fontDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _font = _fontDialog.Font;
+                Font = _font;
+            }
+        }
+    }
+}
